6a5a27203aec35f15fcfbeb6525e81006f0a452f
gelacioangeles <gelacioangeleslp@gmail.com>
2024-11-04 21:25:58 -0600

    Updating script.js
    Adding in the initMap function so I can initialize the map centered on IIT using latitude and longitude. I also added in a zoom level that changes how close the map appears.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 let map, infoWindow;
+
+function initMap() {
+  // Initializing the map centered on IIT
+  const IIT = { lat: 41.831299, lng: -87.627274 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: IIT,
+    zoom: 10,
+  });
