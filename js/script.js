18237e260a7f997dfbfedb4b1bba57a2e9ee1cb3
gelacioangeles <gelacioangeleslp@gmail.com>
2024-11-04 21:27:45 -0600

    Updating script.js
    Fixing my comments and also added in a marker for IIT

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 let map, infoWindow;
 
 function initMap() {
-  // Initializing the map centered on IIT
+  // Initializing the map centered on IIT.
   const IIT = { lat: 41.831299, lng: -87.627274 };
   map = new google.maps.Map(document.getElementById("map"), {
     center: IIT,
     zoom: 10,
+  });
+  
+  // Creating a marker for IIT.
+  const markerIIT = new google.maps.Marker({
+    position: IIT,
+    map,
+    title: "Illinois Institute of Technology",
   });
